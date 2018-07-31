ac0fb97a6babdd2f9f00acaabd1278f5491afd17
Liam Clarke <40211863+onlyliam@users.noreply.github.com>
2018-07-31 19:28:55 +0100

    JS update

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+
 //This variable is to keep track of score.
 var score = 0;
 
@@ -7,9 +8,11 @@ and how it handles the users input to generate
 their score.
 */
 var questionOne = prompt("What is the name of SEGA's blue mascot?");
-var questionOneAnswer = "SONIC";
-if (questionOne.toUpperCase() === questionOneAnswer) {
-  score += +1;
+var questionOneAnswerA = "SONIC";
+var questionOneAnswerB = "SONIC THE HEDGEHOG";
+
+if (questionOne.toUpperCase() === questionOneAnswerA || questionOneAnswerB ) {
+  score += 1;
   alert("That is correct!");
 } else {
   alert("That is incorrect!");
@@ -23,7 +26,7 @@ their score.
 var questionTwo = prompt("Where did Gordon Freeman work?");
 var questionTwoAnswer = "BLACK MESA";
 if (questionTwo.toUpperCase() === questionTwoAnswer) {
-  score += +1;
+  score += 1;
   alert("That is correct!");
 } else {
   alert("That is incorrect!");
@@ -37,7 +40,7 @@ their score.
 var questionThree = prompt("What colour is Megaman's friend, Protoman?");
 var questionThreeAnswer = "RED";
 if (questionThree.toUpperCase() === questionThreeAnswer) {
-  score += +1;
+  score += 1;
   alert("That is correct!");
 } else {
   alert("That is incorrect!");
@@ -51,7 +54,7 @@ their score.
 var questionFour = prompt("What is the name of Ryo Hazuki's hometown?");
 var questionFourAnswer = "YAMANOSE";
 if (questionFour.toUpperCase() === questionFourAnswer) {
-  score += +1;
+  score += 1;
   alert("That is correct!");
 } else {
   alert("That is incorrect!");
@@ -63,9 +66,11 @@ and how it handles the users input to generate
 their score.
 */
 var questionFive = prompt("What is Ness's main weapon?");
-var questionFiveAnswer = "BASEBALL BAT";
-if (questionFive.toUpperCase() === questionFiveAnswer) {
-  score += +1;
+var questionFiveAnswerA = "BASEBALL BAT";
+var questionFiveAnswerB = "A BASEBALL BAT";
+
+if (questionFive.toUpperCase() === questionFiveAnswerA || questionFiveAnswerB ) {
+  score += 1;
   alert("That is correct!");
 } else {
   alert("That is incorrect!");
